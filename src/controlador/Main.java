1cd2620bb993537aa75ae57ab80b4917158d7e7a
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-14 16:46:24 -0300

    quite comentarios inecesarios

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package controlador;
 
 /**
@@ -14,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Preubas para la etapa 1
     }
     
 }
