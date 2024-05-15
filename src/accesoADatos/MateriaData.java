40f0c5ba407c7422d135b50d49425c95161f6dc8
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-15 18:45:35 -0300

    Update MateriaData.java

--- src/accesoADatos/MateriaData.java
+++ src/accesoADatos/MateriaData.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import accesoADatos.Conexion;
+
 import javax.swing.JOptionPane;
 
 /**
