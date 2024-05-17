b1fd7b115b696f73204f1d5b1d97f225bcf601ed
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-17 18:20:50 -0300

    Update Main.java

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -17,6 +17,15 @@ public class Main {
      */
     public static void main(String[] args) {
      
+        AlumnoData aldata=new AlumnoData();
+        
+        aldata.listarAlumnos();
+        
+        
+        
+        //
+        Conexion.mostrarErrores();
+        
     }
     
 }
