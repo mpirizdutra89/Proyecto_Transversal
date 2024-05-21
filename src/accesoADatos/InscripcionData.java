d1bf1d13974f380993ca78765dd8e39d2f166815
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-21 14:54:49 -0300

    Update InscripcionData.java

--- src/accesoADatos/InscripcionData.java
+++ src/accesoADatos/InscripcionData.java
@@ -1,9 +1,31 @@
 package accesoADatos;
+import java.sql.Connection;
+import entidades.Inscripcion;
+import accesoADatos.Conexion;
+import entidades.Alumno;
+import entidades.Materia;
+import java.sql.Connection;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
 /**
  *
  * @author agus1
  */
 public class InscripcionData {
-    
+  private static Inscripcion inscripcion;
+
+    private MateriaData mateData;
+    private AlumnoData alumData;
+    private static Connection conec = null;
+
+    public InscripcionData() {
+        conec = Conexion.getConexion();
+    }
 }
