5d9cf87fc95be0ba815896b42cc205dc654af0a2
FerrandoCarlos <cferrando2015@gmail.com>
2024-05-21 22:20:22 -0300

    Update MateriaData.java

--- src/accesoADatos/MateriaData.java
+++ src/accesoADatos/MateriaData.java
@@ -4,8 +4,6 @@ import java.sql.*;
 import entidades.Materia;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
